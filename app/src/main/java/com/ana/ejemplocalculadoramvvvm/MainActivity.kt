931914a0e8d23671b075603e786543139a420bc4
anamvelasco <anitamar2@gmail.com>
2023-09-18 00:27:53 -0500

    Se hacen los cambios respectivos del ViewModel y de los LiveData

--- app/src/main/java/com/ana/ejemplocalculadoramvvvm/MainActivity.kt
+++ app/src/main/java/com/ana/ejemplocalculadoramvvvm/MainActivity.kt
@@ -2,8 +2,6 @@ package com.ana.ejemplocalculadoramvvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ana.ejemplocalculadoramvvvm.databinding.ActivityMainBinding
@@ -22,34 +20,63 @@ class MainActivity : AppCompatActivity() {
 
         mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-        val sumaObserver = Observer<Double> {suma ->
-            mainBinding.resutTextView.setText(suma.toString())
+        // Se hacen los observers para cada operación
+        val sumaObserver = Observer<Double> { resultado ->
+            mainBinding.resutTextView.setText(resultado.toString())
+        }
+        val restaObserver = Observer<Double> { resultado ->
+            mainBinding.resutTextView.setText(resultado.toString())
+        }
+        val multiplicacionObserver = Observer<Double> { resultado ->
+            mainBinding.resutTextView.setText(resultado.toString())
+        }
+        val divisionObserver = Observer<Double> { resultado ->
+            mainBinding.resutTextView.setText(resultado.toString())
+        }
+
+        // Observers para mensajes de error
+        val errorMsgObserver = Observer<String> { errorMsg ->
+            Snackbar.make(view, errorMsg, Snackbar.LENGTH_INDEFINITE)
+                .setAction("Aceptar") {}
+                .show()
         }
 
         mainViewModel.suma.observe(this, sumaObserver)
+        mainViewModel.resta.observe(this, restaObserver)
+        mainViewModel.multiplicacion.observe(this, multiplicacionObserver)
+        mainViewModel.division.observe(this, divisionObserver)
+        mainViewModel.errorMsg.observe(this, errorMsgObserver)
 
-        val errorMsgObserver = Observer<String> {errorMsg ->
-            Snackbar.make(view, errorMsg, Snackbar.LENGTH_INDEFINITE)
-                .setAction("Aceptar"){}
-                .show()
-        }
-        mainViewModel.errorMsg.observe(this,errorMsgObserver)
         mainBinding.addButton.setOnClickListener {
-            mainViewModel.validateNumbers(mainBinding.firstNumberEditText.text.toString(), mainBinding.secondNumberEditText.text.toString())
-           /* if (mainBinding.firstNumberEditText.text.toString().isEmpty() || mainBinding.secondNumberEditText.text.toString().isEmpty()){
-                //Snackbar.make(view,"Debe digitar dos números", Snackbar.LENGTH_SHORT).show()
-                Snackbar.make(view,"Debe digitar dos números", Snackbar.LENGTH_SHORT)
-                    .setAction("Aceptar"){ Log.d("click", "OK")}
-                    .show()
-            } else {
-                val number1 = mainBinding.firstNumberEditText.text.toString().toDouble()
-                val number2 = mainBinding.secondNumberEditText.text.toString().toDouble()
-                val total = number1 + number2
-                mainBinding.resutTextView.setText(total.toString())
-                //mainBinding.resutTextView.text = total.toString()
-            }*/
+            mainViewModel.validateNumbers(
+                mainBinding.firstNumberEditText.text.toString(),
+                mainBinding.secondNumberEditText.text.toString(),
+                "+"
+            )
+        }
 
+        mainBinding.substractButton.setOnClickListener {
+            mainViewModel.validateNumbers(
+                mainBinding.firstNumberEditText.text.toString(),
+                mainBinding.secondNumberEditText.text.toString(),
+                "-"
+            )
+        }
 
+        mainBinding.multiplyButton.setOnClickListener {
+            mainViewModel.validateNumbers(
+                mainBinding.firstNumberEditText.text.toString(),
+                mainBinding.secondNumberEditText.text.toString(),
+                "*"
+            )
+        }
+
+        mainBinding.divideButton.setOnClickListener {
+            mainViewModel.validateNumbers(
+                mainBinding.firstNumberEditText.text.toString(),
+                mainBinding.secondNumberEditText.text.toString(),
+                "/"
+            )
         }
     }
 }
--- app/src/main/java/com/ana/ejemplocalculadoramvvvm/MainViewModel.kt
+++ app/src/main/java/com/ana/ejemplocalculadoramvvvm/MainViewModel.kt
@@ -1,27 +1,51 @@
 package com.ana.ejemplocalculadoramvvvm
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    //Opcion 1
-    //Guardando en feature/practica2
-    val suma : MutableLiveData<Double> by lazy {
+    //Se hace el ViewModel para la suma
+    val suma: MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>()
+    }
+   //Se hace el ViewModel para la resta
+    val resta: MutableLiveData<Double> by lazy {
         MutableLiveData<Double>()
     }
 
-    //Opcion 2
+    //Se hace el ViewModel para la Multiplicación
+    val multiplicacion: MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>()
+    }
+
+    //Se hace el ViewModel para la División
+    val division: MutableLiveData<Double> by lazy {
+        MutableLiveData<Double>()
+    }
+
     private val _errorMsg: MutableLiveData<String> = MutableLiveData()
     val errorMsg: LiveData<String> = _errorMsg
 
-    fun validateNumbers(number1: String, number2: String) {
+    fun validateNumbers(number1: String, number2: String, operation: String) {
         if (number1.isEmpty() || number2.isEmpty()) {
             _errorMsg.value = "Debe digitar todos los números"
         } else {
-            suma.value = number1.toDouble() + number2.toDouble()
+            //Se asignan las operaciones requeridas
+            when (operation) {
+                "+" -> suma.value = number1.toDouble() + number2.toDouble()
+                "-" -> resta.value = number1.toDouble() - number2.toDouble()
+                "*" -> multiplicacion.value = number1.toDouble() * number2.toDouble()
+                //Condición para que no se pueda dividir entre cero
+                "/" -> {
+                    if (number2.toDouble() != 0.0) {
+                        division.value = number1.toDouble() / number2.toDouble()
+                    } else {
+                        _errorMsg.value = "No se puede dividir por cero"
+                    }
+                }
+                else -> _errorMsg.value = "Operación no válida"
+            }
         }
     }
-
 }
